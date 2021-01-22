2e66cddec6a380047f17a0900696ffc03bb54409
Sagun Bhandari <sagun12345m>
2021-01-22 15:55:12 +0545

    third commit

--- app/src/main/java/com/sagun/finalassignment/MainActivity.kt
+++ app/src/main/java/com/sagun/finalassignment/MainActivity.kt
@@ -5,37 +5,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var etfirstname: EditText
-    private   lateinit var etlastname: EditText
-    private lateinit var etusername: EditText
-    private  lateinit var etpassword: EditText
-    private lateinit var btnsignup: Button
-    private lateinit var btnlogin: Button
+//    private lateinit var etfirstname: EditText
+//    private   lateinit var etlastname: EditText
+//    private lateinit var etusername: EditText
+//    private  lateinit var etpassword: EditText
+//    private lateinit var btnsignup: Button
+//    private lateinit var btnlogin: Button
+    private lateinit var signup:TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        etfirstname = findViewById(R.id.etFirstname)
-        etlastname = findViewById(R.id.etLastname)
-        etusername = findViewById(R.id.etUsername)
-        etpassword = findViewById(R.id.etPassword)
-        btnsignup = findViewById(R.id.btnSignUp)
-        btnlogin = findViewById(R.id.btnLogin)
+//        etfirstname = findViewById(R.id.etFirstname)
+//        etlastname = findViewById(R.id.etLastname)
+//        etusername = findViewById(R.id.etUsername)
+//        etpassword = findViewById(R.id.etPassword)
+//        btnsignup = findViewById(R.id.btnSignUp)
+//        btnlogin = findViewById(R.id.btnLogin)
+        signup = findViewById(R.id.signup)
 
-
-        btnsignup.setOnClickListener {
-            try {
-                val intent = Intent(this, SignupActivity::class.java)
-                startActivity(intent)
-            } catch (ex: Exception) {
-                Toast.makeText(this, ex.message, Toast.LENGTH_LONG).show()
-            }
+        signup.setOnClickListener {
+            startActivity(Intent(this,SignupActivity::class.java))
         }
+
+
+//        btnsignup.setOnClickListener {
+//            try {this,
+//                val intent = Intent(this, SignupActivity::class.java)
+//                startActivity(intent)
+//            } catch (ex: Exception) {
+//                Toast.makeText(this, ex.message, Toast.LENGTH_LONG).show()
+//            }
+//        }
     }
 }
