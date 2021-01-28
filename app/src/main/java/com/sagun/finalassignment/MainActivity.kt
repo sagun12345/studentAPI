8c271835e7f509d50cf7790c351c3b4e49310a6e
Sagun Bhandari <sagun12345m>
2021-01-28 14:18:10 +0545

    build.gradle

--- app/src/main/java/com/sagun/finalassignment/MainActivity.kt
+++ app/src/main/java/com/sagun/finalassignment/MainActivity.kt
@@ -10,12 +10,7 @@ import android.widget.Toast
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var etfirstname: EditText
-//    private   lateinit var etlastname: EditText
-//    private lateinit var etusername: EditText
-//    private  lateinit var etpassword: EditText
-//    private lateinit var btnsignup: Button
-//    private lateinit var btnlogin: Button
+
     private lateinit var signup:TextView
 
 
@@ -23,12 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        etfirstname = findViewById(R.id.etFirstname)
-//        etlastname = findViewById(R.id.etLastname)
-//        etusername = findViewById(R.id.etUsername)
-//        etpassword = findViewById(R.id.etPassword)
-//        btnsignup = findViewById(R.id.btnSignUp)
-//        btnlogin = findViewById(R.id.btnLogin)
+
         signup = findViewById(R.id.signup)
 
         signup.setOnClickListener {
@@ -36,13 +26,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-//        btnsignup.setOnClickListener {
-//            try {this,
-//                val intent = Intent(this, SignupActivity::class.java)
-//                startActivity(intent)
-//            } catch (ex: Exception) {
-//                Toast.makeText(this, ex.message, Toast.LENGTH_LONG).show()
-//            }
-//        }
+//
     }
 }
--- app/src/main/java/com/sagun/finalassignment/SignupActivity.kt
+++ app/src/main/java/com/sagun/finalassignment/SignupActivity.kt
@@ -1,15 +1,46 @@
 package com.sagun.finalassignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 
 class SignupActivity : AppCompatActivity() {
 
-
+    private lateinit var etfirstname: EditText
+    private   lateinit var etlastname: EditText
+    private lateinit var etusername: EditText
+    private  lateinit var etpassword: EditText
+    private lateinit var btnsignup: Button
+    private lateinit var btnlogin: Button
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_signup)
+            etfirstname = findViewById(R.id.etFirstname)
+       etlastname = findViewById(R.id.etLastname)
+        etusername = findViewById(R.id.etUsername)
+        etpassword = findViewById(R.id.etPassword)
+       btnsignup = findViewById(R.id.btnSignUp)
+       btnlogin = findViewById(R.id.btnLogin)
+
+            btnsignup.setOnClickListener {
+                val fname = etfirstname.text.toString()
+                val lname = etlastname.text.toString()
+                val username = etusername.text.toString()
+                val password = etpassword.text.toString()
+
+
+
+
+            try {this
+                val intent = Intent(this, SignupActivity::class.java)
+               startActivity(intent)
+            } catch (ex: Exception) {
+                Toast.makeText(this, ex.message, Toast.LENGTH_LONG).show()
+           }
         }
+        }
+
+
     }
