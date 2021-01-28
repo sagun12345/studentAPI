c8f4662966b6722fa5b72813a048788302217476
Sagun Bhandari <sagun12345m>
2021-01-28 14:24:05 +0545

    User

---
+++ app/src/main/java/com/sagun/finalassignment/entity/User.kt
@@ -0,0 +1,17 @@
+package com.sagun.finalassignment.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+class User {
+    @Entity
+    data class User(
+        var fname: String? = null,
+        var lname: String? = null,
+        var username: String? = null,
+        var password: String? = null
+    ) {
+        @PrimaryKey(autoGenerate = true)
+        var userId: Int = 0
+    }
+}
