19ac7b9cea22dbed9f39e22ceeeb321f0c711ec3
Sagun Bhandari <sagun12345m>
2021-01-28 15:49:07 +0545

    Confirm password

--- app/src/main/java/com/sagun/finalassignment/SignupActivity.kt
+++ app/src/main/java/com/sagun/finalassignment/SignupActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SignupActivity : AppCompatActivity() {
 
@@ -18,11 +21,11 @@ class SignupActivity : AppCompatActivity() {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_signup)
             etfirstname = findViewById(R.id.etFirstname)
-       etlastname = findViewById(R.id.etLastname)
-        etusername = findViewById(R.id.etUsername)
-        etpassword = findViewById(R.id.etPassword)
-       btnsignup = findViewById(R.id.btnSignUp)
-       btnlogin = findViewById(R.id.btnLogin)
+            etlastname = findViewById(R.id.etLastname)
+            etusername = findViewById(R.id.etUsername)
+            etpassword = findViewById(R.id.etPassword)
+            btnsignup = findViewById(R.id.btnSignUp)
+            btnlogin = findViewById(R.id.btnLogin)
 
             btnsignup.setOnClickListener {
                 val fname = etfirstname.text.toString()
@@ -30,16 +33,21 @@ class SignupActivity : AppCompatActivity() {
                 val username = etusername.text.toString()
                 val password = etpassword.text.toString()
 
+                if (password != confirmPassword) {
+                    etPassword.error = "Password does not match"
+                    etPassword.requestFocus()
+                    return@setOnClickListener
+                } else {
+                    val user = User(fname, lname, username, password)
+                    CoroutineScope(Dispatchers.IO).launch {
+                        //StudentDB(this@RegisterUserActivity).getUserDAO().registerUser(user)
+                        StudentDB.getInstance(this@RegisterUserActivity).getUserDAO().registerUser(user)
+                    }
+                    Toast.makeText(this, "User registered", Toast.LENGTH_SHORT).show()
+                }
 
+            }
 
-
-            try {this
-                val intent = Intent(this, SignupActivity::class.java)
-               startActivity(intent)
-            } catch (ex: Exception) {
-                Toast.makeText(this, ex.message, Toast.LENGTH_LONG).show()
-           }
-        }
         }
 
 
