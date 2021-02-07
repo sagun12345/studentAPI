c3991110268cf67ea5f47db444d663e198fd7148
Sagun Bhandari <sagun12345m>
2021-02-07 12:05:08 +0545

    ServiceBuilder

---
+++ app/src/main/java/com/sagun/finalassignment/API/ServiceBuilder.kt
@@ -0,0 +1,29 @@
+package com.sagun.finalassignment.API
+
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceBuilder {
+    private const val BASE_URL =
+        "http://10.0.2.2:3000/"
+    var token: String? = null
+
+    private val okHttp =
+        OkHttpClient.Builder()
+
+    //create retrofit builder
+    private val retrofitBuilder = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttp.build())
+
+    //create retrofit instance
+    private val retrofit= retrofitBuilder.build()
+
+    //generic function
+    fun <T> buildService(serviceType: Class<T>): T {
+        return retrofit.create(serviceType)
+    }
+}
