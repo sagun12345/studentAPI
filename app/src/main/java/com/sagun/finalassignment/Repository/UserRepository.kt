410e839226805334d43f5307072f4f5f3aabeb4a
Sagun Bhandari <sagun12345m>
2021-02-07 12:22:29 +0545

    UserRepository

---
+++ app/src/main/java/com/sagun/finalassignment/Repository/UserRepository.kt
@@ -0,0 +1,25 @@
+package com.sagun.finalassignment.Repository
+
+import com.sagun.finalassignment.API.MyApiRequest
+import com.sagun.finalassignment.API.ServiceBuilder
+import com.sagun.finalassignment.entity.User
+import com.sagun.finalassignment.response.LoginResponse
+import com.sagun.finalassignment.API.UserAPI
+class UserRepository :
+    MyApiRequest(){
+    private val UserAPI = ServiceBuilder.buildService(UserAPI::class.java)
+
+    //Register User
+
+    suspend fun registerUser(user: User) : LoginResponse {
+        return apiRequest {
+            UserAPI.registerUser(user)
+        }
+    }
+    //Login User
+    suspend fun loginUser(email : String,password : String) : LoginResponse{
+        return apiRequest {
+            UserAPI.checkUser(email,password)
+        }
+    }
+}
