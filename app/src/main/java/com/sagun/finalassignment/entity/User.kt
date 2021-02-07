07b68cab67fa314568b8e1558e6d0a6db7c81762
Sagun Bhandari <sagun12345m>
2021-02-07 12:01:49 +0545

    MyAPiREQUEST

--- app/src/main/java/com/sagun/finalassignment/entity/User.kt
+++ app/src/main/java/com/sagun/finalassignment/entity/User.kt
@@ -5,12 +5,10 @@ import androidx.room.PrimaryKey
 
 @Entity
     data class User(
+        var _id:String? = null,
         var fname: String? = null,
         var lname: String? = null,
         var username: String? = null,
         var password: String? = null
-    ) {
-        @PrimaryKey(autoGenerate = true)
-        var userId: Int = 0
-    }
+    )
 
